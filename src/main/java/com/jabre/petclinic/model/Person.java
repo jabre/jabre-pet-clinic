acd79367e5276d57dcc20fe4753ab67db64bb26d
jabre <marko13r@gmail.com>
2021-12-03 19:42:19 +0100

    Adding Pjos. Closes 2#

---
+++ src/main/java/com/jabre/petclinic/model/Person.java
@@ -0,0 +1,27 @@
+package com.jabre.petclinic.model;
+
+/**
+ * @author : Jabre
+ * @created : 12/3/2021, Friday
+ **/
+public class Person {
+
+    private String firstName;
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
