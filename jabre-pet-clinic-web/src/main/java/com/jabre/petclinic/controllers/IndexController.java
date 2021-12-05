2bad92ca2c4c226c4cdc2ee86604fd16a0e357db
jabre <marko13r@gmail.com>
2021-12-05 21:56:24 +0100

    Add Pet Clinic Index Page and Controller. Closes #8

---
+++ jabre-pet-clinic-web/src/main/java/com/jabre/petclinic/controllers/IndexController.java
@@ -0,0 +1,17 @@
+package com.jabre.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Author : Jabre
+ * Created : 12/5/2021, Sunday
+ **/
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "index", "index.html" })
+    public String index() {
+        return "index";
+    }
+}
