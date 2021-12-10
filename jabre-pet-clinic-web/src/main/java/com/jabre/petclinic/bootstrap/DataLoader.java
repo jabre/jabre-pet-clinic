486eda71bfe4b50973a8b88cf4fa31970436fe5f
jabre <marko13r@gmail.com>
2021-12-10 23:00:21 +0100

    Update Map impl to manage setting of ID Property. Closes #36

--- jabre-pet-clinic-web/src/main/java/com/jabre/petclinic/bootstrap/DataLoader.java
+++ jabre-pet-clinic-web/src/main/java/com/jabre/petclinic/bootstrap/DataLoader.java
@@ -4,8 +4,6 @@ import com.jabre.petclinic.model.Owner;
 import com.jabre.petclinic.model.Vet;
 import com.jabre.petclinic.services.OwnerService;
 import com.jabre.petclinic.services.VetService;
-import com.jabre.petclinic.services.map.OwnerServiceMap;
-import com.jabre.petclinic.services.map.VetServiceMap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -28,14 +26,12 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Fiona");
         owner2.setLastName("Glenanne");
 
@@ -44,14 +40,12 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded Owners....");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Sam");
         vet1.setLastName("Axe");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Jessie");
         vet2.setLastName("Porter");
 
