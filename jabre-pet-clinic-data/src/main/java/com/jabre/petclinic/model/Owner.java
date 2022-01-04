d07cedf659ea3596d64f6069364b3737909f983e
jabre <marko13r@gmail.com>
2022-01-04 21:10:35 +0100

    Create Pet Type, Pet and Visit Entities Closes #43

--- jabre-pet-clinic-data/src/main/java/com/jabre/petclinic/model/Owner.java
+++ jabre-pet-clinic-data/src/main/java/com/jabre/petclinic/model/Owner.java
@@ -1,8 +1,20 @@
 package com.jabre.petclinic.model;
 
+import java.util.Set;
+
 /**
  * Author : Jabre
  * Created : 12/3/2021, Friday
  **/
 public class Owner extends Person {
+
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
