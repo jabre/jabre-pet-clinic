18d99c44e2049bdc950db1d34347e6a0c9c3fbcd
jabre <marko13r@gmail.com>
2022-01-04 21:13:33 +0100

    Create Vet Specialty Entity, Associate to Vet Closes #44

---
+++ jabre-pet-clinic-data/src/main/java/com/jabre/petclinic/model/Speciality.java
@@ -0,0 +1,18 @@
+package com.jabre.petclinic.model;
+
+/**
+ * Author : Jabre
+ * Created : 1/4/2022, Tuesday
+ **/
+public class Speciality extends BaseEntity {
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
