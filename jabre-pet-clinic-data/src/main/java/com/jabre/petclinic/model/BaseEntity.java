65738396e42ad3ae94dab76adccd18febe07472c
jabre <jabre@users.noreply.github.com>
2022-02-26 19:30:50 +0100

    Develop (#65)
    * Create Pet Type, Pet and Visit Entities Closes #43
    
    * Create Vet Specialty Entity, Associate to Vet Closes #44
    
    * Add Contact into Properties to Owners Closses #45
    
    * Create Pet Type Map Service Closes #46
    
    * Add Pet Types into Bootstrap (startup) Closes #49
    
    * Enhance Owners with contact info and pets Closes #50
    
    * Create Specialty Map Service Closes #48
    
    * Create Specialties, add to Vets on Bootstrap (startup) Closes #51
    
    * Broken Menu links Closes #62
    
    * Implement base intity with JPA. Closes #52

--- jabre-pet-clinic-data/src/main/java/com/jabre/petclinic/model/BaseEntity.java
+++ jabre-pet-clinic-data/src/main/java/com/jabre/petclinic/model/BaseEntity.java
@@ -1,13 +1,20 @@
 package com.jabre.petclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
 /**
  * Author : Jabre
  * Created : 12/3/2021, Friday
  **/
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
