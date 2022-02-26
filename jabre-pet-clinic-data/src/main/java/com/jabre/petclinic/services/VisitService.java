e367d933ce9ec4e52dfbec9e4685f8f4c6b2387d
jabre <marko13r@gmail.com>
2022-02-26 22:25:26 +0100

    Create a Visit Map Service Closes #75

---
+++ jabre-pet-clinic-data/src/main/java/com/jabre/petclinic/services/VisitService.java
@@ -0,0 +1,10 @@
+package com.jabre.petclinic.services;
+
+import com.jabre.petclinic.model.Visit;
+
+/**
+ * Author : Jabre
+ * Created : 2/26/2022, Saturday
+ **/
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ jabre-pet-clinic-data/src/main/java/com/jabre/petclinic/services/map/VisitServiceMap.java
@@ -0,0 +1,45 @@
+package com.jabre.petclinic.services.map;
+
+import com.jabre.petclinic.model.Visit;
+import com.jabre.petclinic.services.SpecialityService;
+import com.jabre.petclinic.services.VisitService;
+
+import java.util.Set;
+
+/**
+ * Author : Jabre
+ * Created : 2/26/2022, Saturday
+ **/
+public class VisitServiceMap extends AbstractMapService<Visit, Long> implements VisitService {
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        if (visit.getPet() == null
+                || visit.getPet().getOwner() == null
+                || visit.getPet().getId() == null
+                || visit.getPet().getOwner().getId() == null) {
+            throw new RuntimeException("Invalid Visit!");
+        }
+        return super.save(visit);
+    }
+}
