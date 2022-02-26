18c0188ae37585d1cc5000faa11e6ea40b897bc5
jabre <marko13r@gmail.com>
2022-02-26 20:20:39 +0100

    Covert Vets to JPA Entities Closes #54

--- jabre-pet-clinic-data/src/main/java/com/jabre/petclinic/model/Speciality.java
+++ jabre-pet-clinic-data/src/main/java/com/jabre/petclinic/model/Speciality.java
@@ -1,11 +1,18 @@
 package com.jabre.petclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  * Author : Jabre
  * Created : 1/4/2022, Tuesday
  **/
+@Entity
+@Table(name = "specialities")
 public class Speciality extends BaseEntity {
 
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
--- jabre-pet-clinic-data/src/main/java/com/jabre/petclinic/model/Vet.java
+++ jabre-pet-clinic-data/src/main/java/com/jabre/petclinic/model/Vet.java
@@ -1,5 +1,6 @@
 package com.jabre.petclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -7,8 +8,12 @@ import java.util.Set;
  * Author : Jabre
  * Created : 12/3/2021, Friday
  **/
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specialities", joinColumns = @JoinColumn(name = "vet_id"), inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Speciality> specialities = new HashSet<>();
 
     public Set<Speciality> getSpecialities() {
